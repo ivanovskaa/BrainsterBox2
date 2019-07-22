c9f19ea5537772e1824d4dde713cc86f8ffbdb97
Aleksandar Sajdovski <Aleksandar.Sajdovski@gmail.com>
2019-07-22 11:22:35 +0200

    Updates

---
+++ precache-manifest.74b82105f8893595f08bee9ff49a7d32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cdcfe8d26c01f50d6c344d5b41c301",
+    "url": "/BrainsterBox/index.html"
+  },
+  {
+    "revision": "790de7c78502616419bc",
+    "url": "/BrainsterBox/static/css/main.9239040b.chunk.css"
+  },
+  {
+    "revision": "9f163cf77bb4f933ad3c",
+    "url": "/BrainsterBox/static/js/2.baf4b151.chunk.js"
+  },
+  {
+    "revision": "790de7c78502616419bc",
+    "url": "/BrainsterBox/static/js/main.8806abe6.chunk.js"
+  },
+  {
+    "revision": "cc6679132648dda4f6bc",
+    "url": "/BrainsterBox/static/js/runtime~main.8e6b4d05.js"
+  },
+  {
+    "revision": "0f4ef4f15b0685c10671288c4cb62a9f",
+    "url": "/BrainsterBox/static/media/Map.0f4ef4f1.jpg"
+  },
+  {
+    "revision": "dcd3727437d20b4817cd5c9c7787717b",
+    "url": "/BrainsterBox/static/media/logo.dcd37274.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BrainsterBox2/precache-manifest.372ee431653e664c2f77e880e897d097.js"
+  "/BrainsterBox/precache-manifest.74b82105f8893595f08bee9ff49a7d32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BrainsterBox2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BrainsterBox/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
